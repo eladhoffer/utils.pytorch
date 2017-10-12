3f0466a6a7a2cff107fdedc7bac71664d13b5731
Elad Hoffer <elad.hoffer@gmail.com>
2017-10-12 16:48:25 +0300

    remove bkchart dep

--- log.py
+++ log.py
@@ -1,17 +1,14 @@
 import os
+from itertools import cycle
 import torch
 import logging.config
 import shutil
 import pandas as pd
 from bokeh.io import output_file, save, show
 from bokeh.plotting import figure
+from bokeh.plotting.helpers import DEFAULT_PALETTE
 from bokeh.layouts import column
-from bokeh.charts import Line, defaults
 from copy import deepcopy
-
-defaults.width = 800
-defaults.height = 400
-defaults.tools = 'pan,box_zoom,wheel_zoom,box_select,hover,reset,save'
 
 
 def setup_logging(log_file='log.txt'):
@@ -34,8 +31,11 @@ class ResultsLog(object):
     def __init__(self, path='results.csv', plot_path=None):
         self.path = path
         self.plot_path = plot_path or (self.path + '.html')
-        self.figures = []
         self.results = None
+        self.clear()
+
+    def clear(self):
+        self.figures = []
 
     def add(self, **kwargs):
         df = pd.DataFrame([kwargs.values()], columns=kwargs.keys())
@@ -51,7 +51,7 @@ class ResultsLog(object):
             output_file(self.plot_path, title=title)
             plot = column(*self.figures)
             save(plot)
-            self.figures = []
+            self.clear()
         self.results.to_csv(self.path, index=False, index_label=False)
 
     def load(self, path=None):
@@ -64,18 +64,28 @@ class ResultsLog(object):
             plot = column(*self.figures)
             show(plot)
 
-    def plot(self, *kargs, **kwargs):
-        line = Line(data=self.results, *kargs, **kwargs)
-        self.figures.append(line)
+    def plot(self, x, y, title=None, xlabel=None, ylabel=None,
+             width=800, height=400, colors=None, line_width=2,
+             tools='pan,box_zoom,wheel_zoom,box_select,hover,reset,save'):
+        xlabel = xlabel or x
+        f = figure(title=title, tools=tools,
+                   width=width, height=height,
+                   x_axis_label=xlabel or x,
+                   y_axis_label=ylabel or '')
+        if colors is not None:
+            colors = iter(colors)
+        else:
+            colors = cycle(DEFAULT_PALETTE)
+        for yi in y:
+            f.line(self.results[x], self.results[yi],
+                   line_width=line_width,
+                   line_color=next(colors), legend=yi)
+        self.figures.append(f)
 
     def image(self, *kargs, **kwargs):
         fig = figure()
         fig.image(*kargs, **kwargs)
         self.figures.append(fig)
-
-
-
-
 
 
 def save_checkpoint(state, is_best, path='.', filename='checkpoint.pth.tar', save_all=False):

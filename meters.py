1c6a95fb571ba03a88c0aca1b3bf7d7097ab3aa4
Elad Hoffer <elad.hoffer@gmail.com>
2017-10-13 11:21:29 +0300

    fix accuray topk bug

--- meters.py
+++ meters.py
@@ -59,6 +59,7 @@ class OnlineMeter(object):
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
+    batch_size = target.size(0)
 
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t().type_as(target)
@@ -66,8 +67,8 @@ def accuracy(output, target, topk=(1,)):
 
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().mean(0)
-        res.append(correct_k.mul_(100.0))
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
 

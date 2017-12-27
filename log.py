5446a910896f3570fbb57ff459bb6bc7588dd32c
Elad Hoffer <elad.hoffer@gmail.com>
2017-12-27 13:50:20 +0200

    update log

--- log.py
+++ log.py
@@ -9,7 +9,6 @@ import json
 import pandas as pd
 from bokeh.io import output_file, save, show
 from bokeh.plotting import figure
-import bokeh.palettes as pal
 from bokeh.layouts import column
 from bokeh.models import Div
 
@@ -40,7 +39,8 @@ def setup_logging_and_results(args):
     log_file = os.path.join(save_path, 'log.txt')
 
     setup_logging(log_file, resume)
-    results = ResultsLog(path=save_path, plot_title=save_name, resume=False, data_format=data_format)
+    results = ResultsLog(path=save_path, title=save_name,
+                         resume=False, data_format=data_format)
     export_args(args, save_path)
     return results, save_path
 
@@ -86,7 +86,7 @@ class ResultsLog(object):
 
     supported_data_formats = ['csv', 'json']
 
-    def __init__(self, path='', plot_title='', resume=False, data_format='csv'):
+    def __init__(self, path='', title='', resume=False, data_format='csv'):
         """
         Parameters
         ----------
@@ -94,7 +94,7 @@ class ResultsLog(object):
             path to directory to save data files
         plot_path: string
             path to directory to save plot files
-        plot_title: string
+        title: string
             title of HTML file
         resume: bool
             resume previous logging
@@ -105,28 +105,25 @@ class ResultsLog(object):
             raise ValueError('data_format must of the following: ' +
                              '|'.join(['{}'.format(k) for k in ResultsLog.supported_data_formats]))
 
-        os.makedirs(path, exist_ok=True)
-        full_path = os.path.join(path, 'results')
         if data_format == 'json':
-            self.date_path = '{}.json'.format(full_path)
+            self.data_path = '{}.json'.format(path)
         else:
-            self.date_path = '{}.csv'.format(full_path)
-        self.plot_path = '{}.html'.format(full_path)
+            self.data_path = '{}.csv'.format(path)
+        self.plot_path = '{}.html'.format(path)
         self.results = None
         self.clear()
-
         self.first_save = True
-        if os.path.isfile(self.date_path):
+        if os.path.isfile(self.data_path):
             if resume:
-                self.load(self.date_path)
+                self.load(self.data_path)
                 self.first_save = False
             else:
-                os.remove(self.date_path)
+                os.remove(self.data_path)
                 self.results = pd.DataFrame()
         else:
             self.results = pd.DataFrame()
 
-        self.plot_title = plot_title
+        self.title = title
         self.data_format = data_format
 
     def clear(self):
@@ -135,7 +132,8 @@ class ResultsLog(object):
     def add(self, **kwargs):
         """Add a new row to the dataframe
         example:
-            resultsLog.add(epoch=epoch_num, train_loss=loss, test_loss=test_loss)
+            resultsLog.add(epoch=epoch_num, train_loss=loss,
+                           test_loss=test_loss)
         """
         df = pd.DataFrame([kwargs.values()], columns=kwargs.keys())
         self.results = self.results.append(df, ignore_index=True)
@@ -143,32 +141,36 @@ class ResultsLog(object):
     def smooth(self, column_name, window):
         """Select an entry to smooth over time"""
         # TODO: smooth only new data
-        smoothed_column = self.results[column_name].rolling(window=window, center=False).mean()
-        self.results[column_name+'_smoothed'] = smoothed_column
+        smoothed_column = self.results[column_name].rolling(
+            window=window, center=False).mean()
+        self.results[column_name + '_smoothed'] = smoothed_column
 
-    def save(self, title='Training Results'):
+    def save(self, title=None):
         """save the json file.
         Parameters
         ----------
         title: string
             title of the HTML file
         """
+        title = title or self.title
         if len(self.figures) > 0:
             if os.path.isfile(self.plot_path):
                 os.remove(self.plot_path)
             if self.first_save:
                 self.first_save = False
-                logging.info('Plot file saved at: {}'.format(os.path.abspath(self.plot_path)))
+                logging.info('Plot file saved at: {}'.format(
+                    os.path.abspath(self.plot_path)))
 
             output_file(self.plot_path, title=title)
-            plot = column(Div(text='<h1 align="center">{}</h1>'.format(self.plot_title)), *self.figures)
+            plot = column(
+                Div(text='<h1 align="center">{}</h1>'.format(title)), *self.figures)
             save(plot)
             self.clear()
 
         if self.data_format == 'json':
-            self.results.to_json(self.date_path, orient='records', lines=True)
+            self.results.to_json(self.data_path, orient='records', lines=True)
         else:
-            self.results.to_csv(self.date_path, index=False, index_label=False)
+            self.results.to_csv(self.data_path, index=False, index_label=False)
 
     def load(self, path=None):
         """load the data file
@@ -177,7 +179,7 @@ class ResultsLog(object):
         path:
             path to load the json|csv file from
         """
-        path = path or self.path
+        path = path or self.data_path
         if os.path.isfile(path):
             if self.data_format == 'json':
                 self.results.read_json(path)
@@ -186,35 +188,38 @@ class ResultsLog(object):
         else:
             raise ValueError('{} isn''t a file'.format(path))
 
-    def show(self):
+    def show(self, title=None):
+        title = title or self.title
         if len(self.figures) > 0:
-            plot = column(Div(text='<h1 align="center">{}</h1>'.format(self.plot_title)), *self.figures)
+            plot = column(
+                Div(text='<h1 align="center">{}</h1>'.format(title)), *self.figures)
             show(plot)
 
-    def plot(self, x, y, title=None, xlabel=None, ylabel=None,
-             width=800, height=400, colors=None, line_width=2,
+    def plot(self, x, y, title=None, xlabel=None, ylabel=None, legend=None,
+             width=800, height=400, line_width=2,
+             colors=['red', 'green', 'blue', 'orange',
+                     'black', 'purple', 'brown'],
              tools='pan,box_zoom,wheel_zoom,box_select,hover,reset,save'):
         """
         add a new plot to the HTML file
         example:
             results.plot(x='epoch', y=['train_loss', 'val_loss'],
-                         title='Loss', ylabel='loss')
+                         'title='Loss', 'ylabel'='loss')
         """
         if not isinstance(y, list):
             y = [y]
         xlabel = xlabel or x
+        legend = legend or y
+        assert len(legend) == len(y)
         f = figure(title=title, tools=tools,
                    width=width, height=height,
                    x_axis_label=xlabel or x,
                    y_axis_label=ylabel or '')
-        if colors is not None:
-            colors = iter(colors)
-        else:
-            colors = cycle(list(pal.Colorblind[8]))
-        for yi in y:
+        colors = cycle(colors)
+        for i, yi in enumerate(y):
             f.line(self.results[x], self.results[yi],
                    line_width=line_width,
-                   line_color=next(colors), legend=yi)
+                   line_color=next(colors), legend=legend[i])
         self.figures.append(f)
 
     def image(self, *kargs, **kwargs):
@@ -231,3 +236,4 @@ def save_checkpoint(state, is_best, path='.', filename='checkpoint.pth.tar', sav
     if save_all:
         shutil.copyfile(filename, os.path.join(
             path, 'checkpoint_epoch_%s.pth.tar' % state['epoch']))
+

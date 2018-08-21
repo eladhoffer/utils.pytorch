57206b40033e71fb0594aa850dcb72de740e0eea
Elad Hoffer <elad.hoffer@gmail.com>
2018-08-21 15:07:57 +0300

    log plot update

--- log.py
+++ log.py
@@ -57,6 +57,42 @@ def setup_logging(log_file='log.txt', resume=False, dummy=False):
         logging.getLogger('').addHandler(console)
 
 
+def plot_figure(data, x, y, title=None, xlabel=None, ylabel=None, legend=None,
+                x_axis_type='linear', y_axis_type='linear',
+                width=800, height=400, line_width=2,
+                colors=['red', 'green', 'blue', 'orange',
+                        'black', 'purple', 'brown'],
+                tools='pan,box_zoom,wheel_zoom,box_select,hover,reset,save',
+                append_figure=None):
+    """
+    creates a new plot figures
+    example:
+        plot_figure(x='epoch', y=['train_loss', 'val_loss'],
+                        'title='Loss', 'ylabel'='loss')
+    """
+    if not isinstance(y, list):
+        y = [y]
+    xlabel = xlabel or x
+    legend = legend or y
+    assert len(legend) == len(y)
+    if append_figure is not None:
+        f = append_figure
+    else:
+        f = figure(title=title, tools=tools,
+                   width=width, height=height,
+                   x_axis_label=xlabel or x,
+                   y_axis_label=ylabel or '',
+                   x_axis_type=x_axis_type,
+                   y_axis_type=y_axis_type)
+    colors = cycle(colors)
+    for i, yi in enumerate(y):
+        f.line(data[x], data[yi],
+               line_width=line_width,
+               line_color=next(colors), legend=legend[i])
+    f.legend.click_policy = "hide"
+    return f
+
+
 class ResultsLog(object):
 
     supported_data_formats = ['csv', 'json']
@@ -184,35 +220,14 @@ class ResultsLog(object):
                 Div(text='<h1 align="center">{}</h1>'.format(title)), *self.figures)
             show(plot)
 
-    def plot(self, x, y, title=None, xlabel=None, ylabel=None, legend=None,
-             x_axis_type='linear', y_axis_type='linear',
-             width=800, height=400, line_width=2,
-             colors=['red', 'green', 'blue', 'orange',
-                     'black', 'purple', 'brown'],
-             tools='pan,box_zoom,wheel_zoom,box_select,hover,reset,save'):
+    def plot(self, *kargs, **kwargs):
         """
         add a new plot to the HTML file
         example:
             results.plot(x='epoch', y=['train_loss', 'val_loss'],
                          'title='Loss', 'ylabel'='loss')
         """
-        if not isinstance(y, list):
-            y = [y]
-        xlabel = xlabel or x
-        legend = legend or y
-        assert len(legend) == len(y)
-        f = figure(title=title, tools=tools,
-                   width=width, height=height,
-                   x_axis_label=xlabel or x,
-                   y_axis_label=ylabel or '',
-                   x_axis_type=x_axis_type,
-                   y_axis_type=x_axis_type)
-        colors = cycle(colors)
-        for i, yi in enumerate(y):
-            f.line(self.results[x], self.results[yi],
-                   line_width=line_width,
-                   line_color=next(colors), legend=legend[i])
-        f.legend.click_policy = "hide"
+        f = plot_figure(self.results, *kargs, **kwargs)
         self.figures.append(f)
 
     def image(self, *kargs, **kwargs):

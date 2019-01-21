2a8adb76eb69d8873ba3292142f1be6e7be9f138
Elad Hoffer <ehoffer@gaudilabs.com>
2019-01-21 17:53:04 +0200

    add LARS

--- regularization.py
+++ regularization.py
@@ -1,5 +1,6 @@
 import torch
 from .param_filter import FilterParameters, is_not_bn, is_not_bias
+from .absorb_bn import search_absorbe_bn
 from torch.nn.utils import clip_grad_norm_
 import logging
 
@@ -49,10 +50,10 @@ class Regularizer(object):
 
     def post_step(self):
         pass
-    
+
     def pre_forward(self):
         pass
-    
+
     def pre_backward(self):
         pass
 
@@ -75,6 +76,14 @@ class RegularizerList(Regularizer):
     def post_step(self):
         for reg in self.regularization_list:
             reg.post_step()
+
+    def pre_forward(self):
+        for reg in self.regularization_list:
+            reg.pre_forward()
+
+    def pre_backward(self):
+        for reg in self.regularization_list:
+            reg.pre_backward()
 
 
 class L2Regularization(Regularizer):
@@ -173,9 +182,81 @@ class BoundedWeightNorm(Regularizer):
             for n, p in self._named_parameters:
                 self.prev_norms[n] = _norm_exclude_dim(
                     p, self.dim, keepdim=True)
+                p.grad.mul_(p/self.prev_norms[n])
 
     def post_step(self):
         with torch.no_grad():
             for n, p in self._named_parameters:
                 _renorm(p, dim=self.dim, inplace=True)
                 p.mul_(self.prev_norms[n])
+
+
+class LARS(Regularizer):
+    """Large Batch Training of Convolutional Networks - https://arxiv.org/abs/1708.03888
+    """
+
+    def __init__(self, model, value=1, beta=0,
+                 filter={'parameter_name': is_not_bias,
+                         'module': is_not_bn},
+                 **kwargs):
+        super(LARS, self).__init__(model, value, filter=filter, **kwargs)
+        self.beta = beta
+
+    def pre_step(self):
+        with torch.no_grad():
+            for _, p in self._named_parameters:
+                norm = p.norm()
+                grad_norm = p.grad.norm()
+                if self.beta > 0:
+                    grad_norm += self.beta * norm
+                p.grad.mul_(self.value * norm/grad_norm)
+
+
+class DropConnect(Regularizer):
+    def __init__(self, model, value=0,
+                 filter={'parameter_name': is_not_bias,
+                         'module': is_not_bn},
+                 shakeshake=False, **kwargs):
+        super(DropConnect, self).__init__(
+            model, value=value, filter=filter, **kwargs)
+        self.shakeshake = shakeshake
+
+    def _drop_parameters(self):
+        self.parameter_copy = {}
+        with torch.no_grad():
+            for n, p in self._named_parameters:
+                self.parameter_copy[n] = p.clone()
+                torch.nn.functional.dropout(p, self.value,
+                                            training=True, inplace=True)
+
+    def _reassign_parameters(self):
+        with torch.no_grad():
+            for n, p in self._named_parameters:
+                p.copy_(self.parameter_copy.pop(n))
+
+    def pre_forward(self):
+        self._drop_parameters()
+
+    def pre_backward(self):
+        if self.shakeshake:
+            self._reassign_parameters()
+
+    def pre_step(self):
+        if not self.shakeshake:
+            self._reassign_parameters()
+
+
+class AbsorbBN(Regularizer):
+    def __init__(self, model, remove_bn=False):
+        self._model = model
+        for m in model.modules():
+            if isinstance(m, torch.nn.BatchNorm2d):
+                m.momentum = 1
+        self.remove_bn = remove_bn
+        self._removed = False
+
+    def pre_forward(self):
+        if self._removed:
+            return
+        search_absorbe_bn(self._model, remove_bn=self.remove_bn, verbose=False)
+        self._removed = self.remove_bn

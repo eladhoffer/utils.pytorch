ea0c4528a3df5865c38e5b175b9aedfbce7a1ff0
Elad Hoffer <ehoffer@habana.ai>
2019-03-04 10:25:22 +0200

    add from logits option

--- cross_entropy.py
+++ cross_entropy.py
@@ -11,30 +11,36 @@ def _is_long(x):
     return isinstance(x, torch.LongTensor) or isinstance(x, torch.cuda.LongTensor)
 
 
-def cross_entropy(logits, target, weight=None, ignore_index=-100, reduction='mean',
-                  smooth_eps=None, smooth_dist=None):
+def cross_entropy(inputs, target, weight=None, ignore_index=-100, reduction='mean',
+                  smooth_eps=None, smooth_dist=None, from_logits=True):
     """cross entropy loss, with support for target distributions and label smoothing https://arxiv.org/abs/1512.00567"""
     smooth_eps = smooth_eps or 0
 
     # ordinary log-liklihood - use cross_entropy from nn
     if _is_long(target) and smooth_eps == 0:
-        return F.cross_entropy(logits, target, weight, ignore_index=ignore_index, reduction=reduction)
+        if from_logits:
+            return F.cross_entropy(inputs, target, weight, ignore_index=ignore_index, reduction=reduction)
+        else:
+            return F.nll_loss(inputs, target, weight, ignore_index=ignore_index, reduction=reduction)
+
+    if from_logits:
+        # log-softmax of inputs
+        lsm = F.log_softmax(inputs, dim=-1)
+    else:
+        lsm = inputs
 
     masked_indices = None
-    num_classes = logits.size(-1)
+    num_classes = inputs.size(-1)
 
     if _is_long(target) and ignore_index >= 0:
         masked_indices = target.eq(ignore_index)
 
     if smooth_eps > 0 and smooth_dist is not None:
         if _is_long(target):
-            target = onehot(target, num_classes).type_as(logits)
+            target = onehot(target, num_classes).type_as(inputs)
         if smooth_dist.dim() < target.dim():
             smooth_dist = smooth_dist.unsqueeze(0)
         target.lerp_(smooth_dist, smooth_eps)
-
-    # log-softmax of logits
-    lsm = F.log_softmax(logits, dim=-1)
 
     if weight is not None:
         lsm = lsm * weight.unsqueeze(0)
@@ -42,8 +48,8 @@ def cross_entropy(logits, target, weight=None, ignore_index=-100, reduction='mea
     if _is_long(target):
         eps_sum = smooth_eps / (num_classes - 1.)
         eps_nll = 1. - eps_sum - smooth_eps
-        lsm_t = lsm.gather(dim=-1, index=target.unsqueeze(-1)).squeeze(-1)
-        loss = -(eps_nll * lsm_t + eps_sum * lsm.sum(-1))
+        likelihood = lsm.gather(dim=-1, index=target.unsqueeze(-1)).squeeze(-1)
+        loss = -(eps_nll * likelihood + eps_sum * lsm.sum(-1))
     else:
         loss = -(target * lsm).sum(-1)
 
@@ -64,13 +70,16 @@ def cross_entropy(logits, target, weight=None, ignore_index=-100, reduction='mea
 class CrossEntropyLoss(nn.CrossEntropyLoss):
     """CrossEntropyLoss - with ability to recieve distrbution as targets, and optional label smoothing"""
 
-    def __init__(self, weight=None, ignore_index=-100, reduction='mean', smooth_eps=None, smooth_dist=None):
+    def __init__(self, weight=None, ignore_index=-100, reduction='mean', smooth_eps=None, smooth_dist=None, from_logits=True):
         super(CrossEntropyLoss, self).__init__(weight=weight,
                                                ignore_index=ignore_index, reduction=reduction)
         self.smooth_eps = smooth_eps
         self.smooth_dist = smooth_dist
+        self.from_logits = from_logits
 
     def forward(self, input, target, smooth_dist=None):
         if smooth_dist is None:
             smooth_dist = self.smooth_dist
-        return cross_entropy(input, target, self.weight, self.ignore_index, self.reduction, self.smooth_eps, smooth_dist)
+        return cross_entropy(input, target, weight=self.weight, ignore_index=self.ignore_index,
+                             reduction=self.reduction, smooth_eps=self.smooth_eps,
+                             smooth_dist=smooth_dist, from_logits=self.from_logits)

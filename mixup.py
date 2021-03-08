acdc51f89a2ff1cb886e48c4a1026b9879410241
Elad Hoffer <elad.hofer@gmail.com>
2021-03-08 10:51:57 +0200

    fix mixup

--- mixup.py
+++ mixup.py
@@ -6,38 +6,27 @@ from torch.nn.functional import one_hot
 
 
 class MixUp(nn.Module):
-    def __init__(self, batch_dim=0):
+    def __init__(self, alpha=1., batch_dim=0):
         super(MixUp, self).__init__()
         self.batch_dim = batch_dim
+        self.alpha = alpha
         self.reset()
 
     def reset(self):
-        self.enabled = False
-        self.mix_values = None
+        self.mix_value = None
         self.mix_index = None
 
     def mix(self, x1, x2):
-        if not torch.is_tensor(self.mix_values):  # scalar
-            return x2.lerp(x1, self.mix_values)
-        else:
-            view = [1] * int(x1.dim())
-            view[self.batch_dim] = -1
-            mix_val = self.mix_values.to(device=x1.device).view(*view)
-            return mix_val * x1 + (1.-mix_val) * x2
+        return x2.lerp(x1, self.mix_value)
 
-    def sample(self, alpha, batch_size, sample_batch=False):
+    def sample(self, batch_size, alpha=None):
+        alpha = self.alpha if alpha is None else alpha
         self.mix_index = torch.randperm(batch_size)
-        if sample_batch:
-            values = beta(alpha, alpha, size=batch_size)
-            self.mix_values = torch.tensor(values, dtype=torch.float)
-        else:
-            self.mix_values = torch.tensor([beta(alpha, alpha)],
-                                           dtype=torch.float)
+        self.mix_value = beta(alpha, alpha)
 
     def mix_target(self, y, n_class):
         if not self.training or \
-            self.mix_values is None or\
-                self.mix_values is None:
+                self.mix_value is None:
             return y
         y = one_hot(y, n_class).to(dtype=torch.float)
         idx = self.mix_index.to(device=y.device)
@@ -46,8 +35,7 @@ class MixUp(nn.Module):
 
     def forward(self, x):
         if not self.training or \
-            self.mix_values is None or\
-                self.mix_values is None:
+                self.mix_value is None:
             return x
         idx = self.mix_index.to(device=x.device)
         x_mix = x.index_select(self.batch_dim, idx)
@@ -74,28 +62,27 @@ def rand_bbox(size, lam):
 
 
 class CutMix(MixUp):
-    def __init__(self, batch_dim=0):
-        super(CutMix, self).__init__(batch_dim)
+    def __init__(self, alpha=1., batch_dim=0):
+        super(CutMix, self).__init__(alpha, batch_dim)
 
     def mix_image(self, x1, x2):
-        assert not torch.is_tensor(self.mix_values) or \
-            self.mix_values.nelement() == 1
-        lam = float(self.mix_values)
+        assert not torch.is_tensor(self.mix_value) or \
+            self.mix_value.nelement() == 1
+        lam = float(self.mix_value)
         bbx1, bby1, bbx2, bby2 = rand_bbox(x1.size(), lam)
         x1[:, :, bbx1:bbx2, bby1:bby2] = x2[:, :, bbx1:bbx2, bby1:bby2]
         lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) /
                    (x1.size()[-1] * x1.size()[-2]))
-        self.mix_values.fill_(lam)
+        self.mix_value.fill_(lam)
         return x1
 
-    def sample(self, alpha, batch_size, sample_batch=False):
-        assert not sample_batch
-        super(CutMix, self).sample(alpha, batch_size, sample_batch)
+    def sample(self, batch_size, alpha=None):
+        super(CutMix, self).sample(batch_size, alpha=alpha)
 
     def forward(self, x):
         if not self.training or \
-            self.mix_values is None or\
-                self.mix_values is None:
+            self.mix_value is None or\
+                self.mix_value is None:
             return x
         idx = self.mix_index.to(device=x.device)
         x_mix = x.index_select(self.batch_dim, idx)
